a3e3173718f11289b867000015f575aecc9faaa3
RightMeProve <officialsatyam2022@gmail.com>
2025-07-03 20:32:36 +0530

    Create MVC file structure

---
+++ 2. mvcs/src/main/java/com/learningSpringBoot/mvcs/mvcs/MvcsApplication.java
@@ -0,0 +1,13 @@
+package com.learningSpringBoot.mvcs.mvcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcsApplication.class, args);
+	}
+
+}
---
+++ 2. mvcs/src/test/java/com/learningSpringBoot/mvcs/mvcs/MvcsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learningSpringBoot.mvcs.mvcs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
