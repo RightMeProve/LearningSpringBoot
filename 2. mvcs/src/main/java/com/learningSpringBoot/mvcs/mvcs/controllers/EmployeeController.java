e885811730f5650f5ed87401cf3ad24766ed4eb6
RightMeProve <officialsatyam2022@gmail.com>
2025-07-14 12:05:46 +0530

    Add service layer

--- 2. mvcs/src/main/java/com/learningSpringBoot/mvcs/mvcs/controllers/EmployeeController.java
+++ 2. mvcs/src/main/java/com/learningSpringBoot/mvcs/mvcs/controllers/EmployeeController.java
@@ -3,6 +3,7 @@ package com.learningSpringBoot.mvcs.mvcs.controllers;
 import com.learningSpringBoot.mvcs.mvcs.dto.EmployeeDTO;
 import com.learningSpringBoot.mvcs.mvcs.entities.EmployeeEntity;
 import com.learningSpringBoot.mvcs.mvcs.repositories.EmployeeRepository;
+import com.learningSpringBoot.mvcs.mvcs.services.EmployeeService;
 import org.springframework.web.bind.annotation.*;
 import java.time.LocalDate;
 import java.util.List;
@@ -11,16 +12,17 @@ import java.util.List;
 @RequestMapping(path = "/employees")
 public class EmployeeController {
 
-    private final EmployeeRepository employeeRepository;
+    private final EmployeeService employeeService;
 
-    public EmployeeController(EmployeeRepository employeeRepository) {
-        this.employeeRepository = employeeRepository;
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
     }
+
 
     @GetMapping(path = "/{employeeId}")
     // to use separate name of input url and code: @PathVariable(name = "employeeId")
-    public EmployeeEntity getEmployeeById(@PathVariable(name = "employeeId") Long id){
-        return employeeRepository.findById(id).orElse(null);
+    public EmployeeDTO getEmployeeById(@PathVariable(name = "employeeId") Long id){
+        return employeeService.findById(id);
     }
 
     // some sample url to test
@@ -29,16 +31,16 @@ public class EmployeeController {
     // http://localhost:8080/employees?
     // http://localhost:8080/employees
     @GetMapping
-    public List<EmployeeEntity> getAllEmployees(@RequestParam(required = false) Integer age, @RequestParam(required = false) String sortBy){
-        return employeeRepository.findAll();
+    public List<EmployeeDTO> getAllEmployees(@RequestParam(required = false) Integer age, @RequestParam(required = false) String sortBy){
+        return employeeService.findAll();
     }
 
     // to check post request, we have to use postman or terminal,as with
     // browser we can't generate post request directly
     @PostMapping
     // with requestBody we can send all the required fields for entry
-    public EmployeeEntity createNewEmployee(@RequestBody EmployeeEntity inputEmployee){
-        return employeeRepository.save(inputEmployee);
+    public EmployeeDTO createNewEmployee(@RequestBody EmployeeDTO inputEmployee){
+        return employeeService.createNewEmployee(inputEmployee);
     }
 
     @PutMapping
